1a4ab412d6eec3a19a5c3fff516e89a9a1faa81f
Noise290 <noisemonster29@gmail.com>
2024-12-17 16:18:53 +0100

    fix kafka test, prevent conflict SecurityConfig with SecurityConfigTest

--- backend/src/test/java/org/noisevisionproductions/portfolio/intergration/config/SecurityConfigTest.java
+++ backend/src/test/java/org/noisevisionproductions/portfolio/intergration/config/SecurityConfigTest.java
@@ -1,9 +1,9 @@
 package org.noisevisionproductions.portfolio.intergration.config;
 
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,12 +12,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
-@Configuration
+@TestConfiguration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfigTest {
 
     @Bean
+    @Primary
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
